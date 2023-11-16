85415382935ebb7298a749a3d2534caa6e1b5311
guytc12 <guytc12@gmail.com>
2023-11-16 23:03:21 +0200

    alot

---
+++ app/retrieve_media.py
@@ -0,0 +1,44 @@
+from google.cloud import storage
+import random
+
+def retrieve_random_video_and_audio_from_bucket(bucket_name, destination_video_path,destination_audio_path, service_account_file):
+    try:
+        # Initialize a GCS client
+        client = storage.Client.from_service_account_json(service_account_file)
+
+        # Get the bucket
+        bucket = client.get_bucket(bucket_name)
+
+        # List all objects in the bucket
+        videos = list(bucket.list_blobs(prefix="videos/",delimiter="/", match_glob="**.mp4"))
+
+        if not videos:
+            print("No videos found in the specified bucket.")
+            return "failed"
+
+        # Choose a random blob (object) from the list
+        random_video = random.choice(videos)
+
+        # Download the random blob to the specified file path
+        random_video.download_to_filename(destination_video_path)
+
+        print(f"Video object '{random_video.name}' retrieved and saved to '{destination_video_path}'.")
+
+        audios = list(bucket.list_blobs(prefix="audios/",delimiter="/", match_glob="**.mp3"))
+
+        if not audios:
+            print("No audios found in the specified bucket.")
+            return
+
+        # Choose a random blob (object) from the list
+        random_audio = random.choice(audios)
+
+        # Download the random blob to the specified file path
+        random_audio.download_to_filename(destination_audio_path)
+
+        print(f"audio object '{random_audio.name}' retrieved and saved to '{destination_audio_path}'.")
+        return "success"
+    except Exception as e:
+        print(f"Error: {e}")
+        return "failed"
+
